5247793005244015f52adea70f281c0271aed546
jain8 <jain85280@gmail.com>
2021-02-04 12:08:34 +0530

    New Sort

---
+++ src/NewSort.java
@@ -0,0 +1,14 @@
+
+import java.util.Arrays;
+class NewSort {
+	public static void main(String args[]) {
+		int data[] = {
+			4, -5, 2, 6, 1
+		}
+		;
+		Arrays.sort(data);
+		for (int c: data) {
+			System.out.println(c);
+		}
+	}
+}
